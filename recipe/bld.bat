8a6c63ed6c5d68bf8281f1307ab00d24f658643c
seanyen <seanyen@microsoft.com>
2019-10-03 20:02:04 -0700

    use nmake instead.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,12 @@
 mkdir build
 cd build
 cmake ^
-    -G "Ninja" ^
+    -G "NMake Makefiles" ^
     -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -D CMAKE_BUILD_TYPE=Release ^
     -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
     -D BUILD_TESTING=FALSE ^
     %SRC_DIR%
 if errorlevel 1 exit 1
-ninja install
+nmake install
 if errorlevel 1 exit 1
