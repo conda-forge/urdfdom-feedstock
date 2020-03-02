c8fce16ddcb6ca4c93c24cf416b05f3660fbb827
seanyen <seanyen@microsoft.com>
2020-03-02 01:50:53 -0800

    updated v2.2.0

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,16 @@
+:: MSVC is preferred.
+set CC=cl.exe
+set CXX=cl.exe
+
 mkdir build
 cd build
 cmake ^
     -G "NMake Makefiles" ^
     -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -D CMAKE_BUILD_TYPE=Release ^
-    -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
     -D BUILD_TESTING=FALSE ^
     %SRC_DIR%
 if errorlevel 1 exit 1
-nmake install
+
+cmake --build . --config Release --target install
 if errorlevel 1 exit 1
